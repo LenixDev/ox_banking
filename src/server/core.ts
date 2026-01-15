b71e0243ed81cac1f8c72a857da0492eb17107c6
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-01-15 11:37:02 -0800

    first step

---
+++ src/server/core.ts
@@ -0,0 +1,6 @@
+const CreateAccount = () => {}
+const GetAccount = () => {}
+const GetCharacterAccount = () => {}
+const GetPlayer = () => {}
+
+export { CreateAccount, GetAccount, GetCharacterAccount, GetPlayer }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
 import type { OxAccountRole, OxAccountUserMetadata } from '@communityox/ox_core';
-import { CreateAccount, GetAccount, GetCharacterAccount, GetPlayer } from '@communityox/ox_core/server';
+import { CreateAccount, GetAccount, GetCharacterAccount, GetPlayer } from './core';
 import { onClientCallback, versionCheck, checkDependency } from '@communityox/ox_lib/server';
 import { oxmysql } from '@communityox/oxmysql';
 import type { DateRange } from 'react-day-picker';
