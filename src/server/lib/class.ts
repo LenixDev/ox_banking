d492e2558a73d5b8901c2449015679a623a6c08e
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-01-25 16:21:55 +0100

    feat(server): add ClassInterface and OxAccount classes for account management
    - Introduce generic ClassInterface for managing class members and keys
    - Implement methods to add and retrieve members within ClassInterface
    - Create OxAccount subclass to represent and manage user accounts
    - Implement async retrieval and validation of accounts using SelectAccount
    - Provide method to set account type to 'shared' via SetAccountType
    - Store instances of OxAccount in static member registry for reuse

---
+++ src/server/lib/class.ts
@@ -0,0 +1,53 @@
+import { SelectAccount, SetAccountType } from './db'
+
+type Dict<T> = { [key: string]: T };
+
+export class ClassInterface {
+  protected static members: Dict<any>;
+  protected static keys?: Dict<Dict<any>>;
+  protected static callableMethods: Dict<true>;
+
+  /** Adds a new member of the class to its registries. */
+  static add(id: string | number, member: any) {
+    if (this.members[id]) return false;
+
+    this.members[id] = member;
+
+    if (this.keys) {
+      Object.entries(this.keys).forEach(([key, obj]) => {
+        if (member[key]) {
+          obj[member[key]] = member;
+        }
+      });
+    }
+
+    return true;
+  }
+  /** Get a member of the class by its id. */
+  static get(id: string | number) {
+    return this.members[id];
+  }
+}
+
+export class OxAccount extends ClassInterface {
+  protected static members: Dict<OxAccount> = {};
+
+  static async get(accountId: number) {
+    if (accountId in this.members) this.members[accountId];
+
+    const validAccount = await SelectAccount(accountId);
+
+    if (!validAccount) throw new Error(`No account exists with accountId ${accountId}.`);
+
+    return new OxAccount(accountId);
+  }
+
+  constructor(public accountId: number) {
+    super();
+    OxAccount.add(accountId, this);
+  }
+
+  async setShared() {
+    return SetAccountType(this.accountId, 'shared');
+  }
+}
