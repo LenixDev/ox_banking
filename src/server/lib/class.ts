dc0fa362ae445b7571490fb68971d7f8429b4259
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-02-04 16:53:02 +0100

    feat(server): add OxPlayer class for player management
    - Introduced OxPlayer class extending ClassInterface
    - Added properties for source, userId, charId, stateId, username, identifier, ped, and private groups
    - Implemented static members and keys dictionaries to track instances
    - Created constructor initializing source and groups
    - Added static get method to retrieve OxPlayer instances by id
    - Developed getGroup method to return group grades based on various filter types
    - Initialized OxPlayer class with static init call

--- src/server/lib/class.ts
+++ src/server/lib/class.ts
@@ -54,6 +54,59 @@ export class ClassInterface {
   }
 }
 
+export class OxPlayer extends ClassInterface {
+  source: number | string;
+  userId: number;
+  charId?: number;
+  stateId?: string;
+  username: string;
+  identifier: string;
+  ped: number;
+  #groups: Dict<number>;
+
+  protected static members: Dict<OxPlayer> = {};
+  protected static keys: Dict<Dict<OxPlayer>> = {
+    userId: {},
+    charId: {},
+  };
+
+  /** Get an instance of OxPlayer with the matching playerId. */
+  static get(id: string | number) {
+    return this.members[id];
+  }
+
+  constructor(source: number) {
+    super();
+    this.source = source;
+    this.#groups = {};
+  }
+
+  /** Returns the current grade of a given group name, or the first matched name and grade in the filter. */
+  getGroup(filter: string): number;
+  getGroup(filter: string[] | Record<string, number>): [string, number] | [];
+  getGroup(filter: string | string[] | Record<string, number>) {
+    if (typeof filter === 'string') {
+      return this.#groups[filter];
+    }
+
+    if (Array.isArray(filter)) {
+      for (const name of filter) {
+        const grade = this.#groups[name];
+        if (grade) return [name, grade];
+      }
+    } else if (typeof filter === 'object') {
+      for (const [name, requiredGrade] of Object.entries(filter)) {
+        const grade = this.#groups[name];
+        if (grade && (requiredGrade as number) <= grade) {
+          return [name, grade];
+        }
+      }
+    }
+  }
+}
+
+OxPlayer.init();
+
 export class OxAccount extends ClassInterface {
   protected static members: Dict<OxAccount> = {};
   
