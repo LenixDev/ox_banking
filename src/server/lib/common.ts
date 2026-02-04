adcfb91379558eb20e612402272b027b0e3605e1
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-02-04 16:00:51 +0100

    feat(common): add locales utility function for typed localization
    - Introduce locales function wrapping locale with type safety
    - Utilize FlattenObjectKeys type for locale keys inference
    - Import locale function from external ox_lib package
    - Export locales function as default for streamlined usage
    - Add Locales type derived from locales.json keys for validation

---
+++ src/server/lib/common.ts
@@ -0,0 +1,6 @@
+import { locale, type FlattenObjectKeys } from '@communityox/ox_lib';
+
+type Locales = FlattenObjectKeys<typeof import('./locales.json')>;
+
+export const locales = <T extends Locales>(str: T, ...args: any[]) => locale(str, ...args) as string;
+export default locales
