db46d1de0ab41582e7acaf0b99d9e9b22a0d5191
LenixDev <149199196+LenixDev@users.noreply.github.com>
2026-02-08 14:33:24 +0100

    refactor(core): remove locales utility and related types
    - Deleted locales function that wrapped locale utility
    - Removed Locales type and related imports
    - Cleaned up unused imports and exports in common.ts
    - Simplified codebase by eliminating redundant locale handling code

--- src/server/core/common.ts
+++
@@ -1,6 +0,0 @@
-import { locale, type FlattenObjectKeys } from '@communityox/ox_lib'
-
-type Locales = FlattenObjectKeys<typeof import('./locales.json')>
-
-export const locales = <T extends Locales>(str: T, ...args: any[]) => locale(str, ...args) as string
-export default locales
